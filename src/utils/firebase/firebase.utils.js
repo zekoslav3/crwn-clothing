a3cfc342ab17b0a4cceecbe9970cd04e43b5e805
zekoslav3 <urospantelic@gmail.com>
2022-04-27 17:34:28 +0200

    update

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -6,6 +6,8 @@ import {
   GoogleAuthProvider,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
 } from 'firebase/auth'
 import {
   getFirestore,
@@ -73,4 +75,10 @@ export const signInAuthUserWithEmailAndPassword = async (email, password) => {
   if (!email || !password) return
 
   return await signInWithEmailAndPassword(auth, email, password)
+}
+
+export const signOutUser = async () => await signOut(auth)
+
+export const onAuthStateChangedListner = (callback) => {
+  onAuthStateChanged(auth, callback)
 }
