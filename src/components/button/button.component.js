4f23de5142c1b4f68b63d19039306b92811a6d1b
zekoslav3 <urospantelic@gmail.com>
2023-03-22 12:19:26 +0100

    commit

--- src/components/button/button.component.js
+++ src/components/button/button.component.js
@@ -1,4 +1,4 @@
-import { BaseButton, GoogleSignInButton, InvertedButton } from './button.styles'
+import { BaseButton, ButtonSpinner, GoogleSignInButton, InvertedButton } from './button.styles'
 
 export const BUTTON_TYPES_CLASSES = {
   base: 'base',
@@ -14,10 +14,13 @@ const getButton = (buttonType = BUTTON_TYPES_CLASSES.base) => (
   }[buttonType]
 )
 
-const Button = ({ children, buttonType, ...otherProps }) =>  {
+const Button = ({ children, buttonType, isLoading, ...otherProps }) => {
   const CustomButton = getButton(buttonType)
-  
-  return <CustomButton {...otherProps}>{children}</CustomButton>
+
+  return (
+    <CustomButton disabled={isLoading} {...otherProps}>
+      {isLoading ? <ButtonSpinner /> : children}
+    </CustomButton>)
 }
 
 export default Button
